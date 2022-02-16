f8f2f6ceec33fda99b398eb1dc4b53e0efae1803
Thanh Nguyen <thanh_it@hotmail.com>
2022-02-16 11:03:19 +0700

    Use resources to reference id

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resource :sleep_records, only: [] do
+      resources :sleep_records, only: [] do
         collection do
           get '', to: 'sleep_records#index'
           post 'create', to: 'sleep_records#create'
         end
 
         member do
-          put ':id/complete', to: 'sleep_records#complete'
-          delete ':id', to: 'sleep_records#delete'
+          put 'complete', to: 'sleep_records#complete'
+          delete '', to: 'sleep_records#delete'
         end
       end
 
-      resource :users, only: [] do
+      resources :users, only: [] do
         collection do
           post 'follow', to: 'users#follow'
           post 'unfollow', to: 'users#unfollow'
